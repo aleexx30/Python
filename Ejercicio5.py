c2316a44e471492bc91ae746a57d211c5008c338
Alex <aalvsor725@ieszaidinvergeles.org>
2025-10-06 12:38:04 +0200

    Ejercicio5

---
+++ Ejercicio5.py
@@ -0,0 +1,6 @@
+horas = float(input("Cuantas horas has trabajado?:"))
+precio = float(input("Cuanto ganas en una hora de tu trabajo?:"))
+
+total = precio*horas
+
+print("Has ganado un total de", total,"€")
