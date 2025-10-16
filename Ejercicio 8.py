d2fec4979c5d7cd7f2777707d0d933f7e5a9fcc6
Alex <aalvsor725@ieszaidinvergeles.org>
2025-10-16 09:35:35 +0200

    Ejercicio 8 *Bien

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -2,6 +2,7 @@ numero1 = int(input("Introduce un número:"))
 numero2 = int(input("Introduce otro número:"))
 
 resultado= int(numero1/numero2)
-print ("El resultado es:",resultado)
 Resto= numero1%numero2
-print ("El Resto es:",Resto)
+
+
+print(numero1,"entre",numero2,"da un cociente",resultado,"y un Resto",Resto,)
