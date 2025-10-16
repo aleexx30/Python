942043b9bd7c38ba7bbec89d7b526dfe01431311
Alex <aalvsor725@ieszaidinvergeles.org>
2025-10-16 10:02:08 +0200

    Ejercicio 12

---
+++ Ejercicio 12.py
@@ -0,0 +1,6 @@
+Barras = int(input("Cuantas barras de pan se han vendido que no son del dia?"))
+normal= round(float(Barras*3.49),2)
+Descuento= round(float((Barras*3.49)*0.60),2)
+
+
+print("El precio normal sería",normal,"Pero con descueto sería",Descuento)
