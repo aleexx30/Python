861c1a15d0903543990e550221a6567652ff535e
Alex <aalvsor725@ieszaidinvergeles.org>
2025-10-16 09:47:48 +0200

    Ejercicio 10

---
+++ Ejercicio 10.py
@@ -0,0 +1,9 @@
+NumeroPayasos = int(input("Cuantos payasos se han vendido?:"))
+NumeroMuñecas = int(input("Cuantas Muñecas se han vendido?:"))
+
+
+
+PesoPayaso = int(NumeroPayasos*112)
+PesoMuñeca = int(NumeroMuñecas*75)
+
+print ("El peso total del paquete será ",PesoPayaso+PesoMuñeca," gramos")
