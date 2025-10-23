e2119602644ae458f69fb4948bd2775663c9e871
Alex <aalvsor725@ieszaidinvergeles.org>
2025-10-23 10:29:39 +0200

    Ejercicio 9 Cadenas

---
+++ Ejercicio 9 Cadenas.py
@@ -0,0 +1,11 @@
+precio = (input("Cuando naciste?:"))
+
+partes= precio.split("/")
+
+dia= partes[0]
+mes= partes[1]
+anyo= partes[2]
+
+print ("Naciste el dia:"+dia)
+print ("Naciste el mes:"+mes)
+print ("Naciste el año:"+anyo)
