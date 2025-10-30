9a64e518a651bd35b967a2698738c32aeb7d0343
Alex <aalvsor725@ieszaidinvergeles.org>
2025-10-30 10:27:24 +0100

    Ejercicio 2 Condicionales

---
+++ Ejercicios Condicionales/Ejercicio 2 Condicionales.py
@@ -0,0 +1,10 @@
+
+contraseña="contraseña"
+
+passwd =input("Cual es tu contraseña?:")
+
+if passwd.lower()==contraseña.lower():
+    print("Las contraseña es correcta")
+else:
+    print("Contraseña incorrecta.")
+
