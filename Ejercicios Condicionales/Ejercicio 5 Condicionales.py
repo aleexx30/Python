89410b6d1da5c134237c114d8b663ebcc53b4cf1
Alex <aalvsor725@ieszaidinvergeles.org>
2025-11-06 09:34:33 +0100

    Ejercicio 5 Condicionales

---
+++ Ejercicios Condicionales/Ejercicio 5 Condicionales.py
@@ -0,0 +1,10 @@
+
+edad= int(input("Cuantos años tienes?:"))
+ingresos= int(input("Cuantos ingresos anuales tienes?:"))
+
+
+
+if edad > 16 and ingresos > 1000:
+    print("Tienes que tributar")
+else:
+    print("no tienes que tributar")
